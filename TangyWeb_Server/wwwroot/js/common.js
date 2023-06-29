8947f2b9c9e238bce13210d331dab116f73c33ae
gerardheuvelman <gerardheuvelman@gmail.com>
2023-06-29 10:59:55 +0200

    52. 4 Assignment 7 - SweetAlert

--- TangyWeb_Server/wwwroot/js/common.js
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -6,3 +6,12 @@
         toastr.error(message, 'Operation Failed.', { timeOut: 5000 });
     }
 } 
+
+window.SwalFire = (type, message) => {
+    if (type === "success") {
+        Swal.fire(message, message, 'success');
+    }
+    else if (type === "error") {
+        Swal.fire(message, message, 'error');
+    }
+} 
