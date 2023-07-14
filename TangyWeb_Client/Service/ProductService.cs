db89a7b48c5c233a2e039c74bcc951809ec4ecbe
gerardheuvelman <gerardheuvelman@gmail.com>
2023-07-14 13:41:46 +0200

    125. Retrieve Products from API

--- TangyWeb_Client/Service/ProductService.cs
+++ TangyWeb_Client/Service/ProductService.cs
@@ -6,20 +6,25 @@ namespace TangyWeb_Client.Service;
 
 public class ProductService : IProductService
 {
-    private readonly HttpClient _httpCient;
+    private readonly HttpClient _httpClient;
+    private IConfiguration _configuration;
+    private string BaseServerUrl;
 
-    public ProductService(HttpClient httpCient)
+    public ProductService(HttpClient httpClient, IConfiguration configuration)
     {
-        _httpCient = httpCient;
-    } 
+        _httpClient = httpClient;
+        _configuration = configuration;
+        BaseServerUrl = _configuration.GetSection("BaseServerUrl").Value;
+    }
 
     public async Task<ProductDTO> Get(int productId)
     {
-        var response = await _httpCient.GetAsync($"/api/product/{productId}");
+        var response = await _httpClient.GetAsync($"/api/product/{productId}");
         var content = await response.Content.ReadAsStringAsync();
         if (response.IsSuccessStatusCode)
         {
             var product = JsonConvert.DeserializeObject<ProductDTO>(content);
+            product.ImageUrl = BaseServerUrl + product.ImageUrl;
             return product;
         }
         else
@@ -27,19 +32,22 @@ public class ProductService : IProductService
             var errorModel = JsonConvert.DeserializeObject<ErrorModelDTO>(content);
             throw new Exception(errorModel.ErrorMessage);
         }
-        
-
     }
 
     public async Task<IEnumerable<ProductDTO>> GetAll()
-    {
-        var response = await _httpCient.GetAsync("/api/product");
-        if (response.IsSuccessStatusCode)
         {
-            var content = await response.Content.ReadAsStringAsync();
-            var products = JsonConvert.DeserializeObject<IEnumerable<ProductDTO>>(content);
-            return products;
+            var response = await _httpClient.GetAsync("/api/product");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var products = JsonConvert.DeserializeObject<IEnumerable<ProductDTO>>(content);
+                foreach(var prod in products)
+                {
+                    prod.ImageUrl=BaseServerUrl+prod.ImageUrl;
+                }
+                return products;
+            }
+
+            return new List<ProductDTO>();
         }
-        return new List<ProductDTO>();
-    }
 }
