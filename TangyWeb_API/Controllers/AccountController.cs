20c8bd503542159d6751a7544d0bb047e4006e84
gerardheuvelman <gerardheuvelman@gmail.com>
2023-07-21 20:08:09 +0200

    171. Sign In Endpoint Part 2

--- TangyWeb_API/Controllers/AccountController.cs
+++ TangyWeb_API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Tangy_Common;
@@ -94,7 +95,31 @@ public class AccountController : ControllerBase
             }
 
             // Everything is valid and we need to log in
+            var signingCredentials = GetSigningCredentials();
+            var claims = await GetClaims(user);
 
+            var tokenOptions = new JwtSecurityToken(
+                issuer: _aPISettings.ValidIssuer,
+                audience: _aPISettings.ValidAudience,
+                claims: claims,
+                expires: DateTime.Now.AddDays(30),
+                signingCredentials: signingCredentials
+                );
+
+            var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+            return Ok(new SignInResponseDTO()
+            {
+                IsAuthSuccessful = true,
+                Token = token,
+                UserDTO = new UserDTO()
+                {
+                    Name = user.Name,
+                    Id = user.Id,
+                    Email = user.Email,
+                    PhoneNumber = user.PhoneNumber
+                }
+            });
         }
         else
         {
@@ -107,11 +132,11 @@ public class AccountController : ControllerBase
         return StatusCode(201);
     }
 
-    private SigningCredentials GetSigningCredentials(string username, string password)
+    private SigningCredentials GetSigningCredentials()
     {
         var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_aPISettings.SecretKey));
 
-        return new SigningCredentials(secret, SecurityAlgorithms.Sha256);
+        return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
     }
 
     private async Task<List<Claim>> GetClaims(ApplicationUser user)
@@ -126,7 +151,7 @@ public class AccountController : ControllerBase
         var roles = await _userManager.GetRolesAsync(await _userManager.FindByEmailAsync(user.Email));
         foreach (var role in roles)
         {
-            claims.Add(new Claim(ClaimTypes.Role, role);
+            claims.Add(new Claim(ClaimTypes.Role, role));
         }
         return claims;
     }
