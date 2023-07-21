27884d8fd6b65edc127f556e455e482547626ac5
gerardheuvelman <gerardheuvelman@gmail.com>
2023-07-21 15:06:55 +0200

    166. SignUp API Endpoint

--- TangyWeb_API/Controllers/AccountController.cs
+++ TangyWeb_API/Controllers/AccountController.cs
@@ -1,26 +1,66 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Tangy_Common;
+using Tangy_DataAccess;
+using Tangy_Models;
 
-namespace TangyWeb_API.Controllers
+namespace TangyWeb_API.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AccountController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AccountController : ControllerBase
-    {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager,
-            RoleManager<IdentityRole> roleManager)
+    public AccountController(
+        UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager,
+        RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _roleManager = roleManager;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SignUp([FromBody]SignUpRequestDTO signUpRequestDTO)
+    {
+        if (signUpRequestDTO is null || !ModelState.IsValid)
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _roleManager = roleManager;
+            return BadRequest();
         }
 
+        var user = new ApplicationUser
+        {
+            UserName = signUpRequestDTO.Email,
+            Email = signUpRequestDTO.Email,
+            Name = signUpRequestDTO.Name,
+            PhoneNumber = signUpRequestDTO.PhoneNumber,
+            EmailConfirmed = true
+        };
+
+        var result = await _userManager.CreateAsync(user, signUpRequestDTO.Password);
+        if (!result.Succeeded)
+        {
+            return BadRequest(new SignUpResponseDTO()
+            {
+                IsRegisterationSuccessful = false,
+                Errors = result.Errors.Select(e=>e.Description)
+            });
+        }
+
+        var roleResult = await _userManager.AddToRoleAsync(user, SD.Role_Customer);
+        if (!roleResult.Succeeded)
+        {
+            return BadRequest(new SignUpResponseDTO()
+            {
+                IsRegisterationSuccessful = false,
+                Errors = result.Errors.Select(e => e.Description)
+            });
+        }
+        return StatusCode(201);
     }
 }
