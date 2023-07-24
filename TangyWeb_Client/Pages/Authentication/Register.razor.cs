12b9388b79961e88203861ee55cabe22f0563667
gerardheuvelman <gerardheuvelman@gmail.com>
2023-07-24 16:40:05 +0200

    183. Splitting code files

---
+++ TangyWeb_Client/Pages/Authentication/Register.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using Tangy_Models;
+using TangyWeb_Client.Service.IService;
+
+namespace TangyWeb_Client.Pages.Authentication;
+
+public partial class Register
+{
+    [Inject]
+    public IAuthenticationService _authService{get; set;}
+
+    [Inject]
+    public NavigationManager _navigationManager {get; set; }
+
+    public bool IsProcessing = false;
+    public IEnumerable<string> Errors { get; set; }
+    public bool ShowRegistrationErrors { get; set; }
+
+    private SignUpRequestDTO SignUpRequest = new();
+
+    private async Task RegisterUser()
+    {
+        ShowRegistrationErrors = false;
+        IsProcessing = true;
+        var result = await _authService.RegisterUser(SignUpRequest);
+        if (result.IsRegistrationSuccessful)
+        {
+            _navigationManager.NavigateTo("/");
+        }
+        else
+        {
+            Errors = result.Errors;
+            ShowRegistrationErrors = true;
+        }
+        IsProcessing = false;
+    }
+}
