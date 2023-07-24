d704dd52b798bbfef061f24f9c23544f03ab4cc7
gerardheuvelman <gerardheuvelman@gmail.com>
2023-07-24 14:34:15 +0200

    178. Login Service

--- TangyWeb_Client/Service/IService/IAuthenticationService.cs
+++ TangyWeb_Client/Service/IService/IAuthenticationService.cs
@@ -4,7 +4,7 @@ namespace TangyWeb_Client.Service.IService;
 
 public interface IAuthenticationService
 {
-    Task<SignUpResponseDTO> RegisterUser(SignUpRequestDTO signUpRequestDTO);
-    Task<SignInResponseDTO> Login(SignInRequestDTO signInRequestDTO);
+    Task<SignUpResponseDTO> RegisterUser(SignUpRequestDTO signUpRequest);
+    Task<SignInResponseDTO> Login(SignInRequestDTO signInRequest);
     Task Logout();
 }
--- Tangy_Common/SD.cs
+++ Tangy_Common/SD.cs
@@ -20,4 +20,5 @@ public static class SD
     public const string Role_Customer = "Customer";
     
     public const string Local_Token = "JWT Token";
+    public const string Local_UserDetails= "UserDetails";
 }
