5f1daa3527db41d48a4e6fd6b1369282d594cdf9
gerardheuvelman <gerardheuvelman@gmail.com>
2023-07-26 16:44:00 +0200

    198. Order Confirmation

--- Tangy_Common/SD.cs
+++ Tangy_Common/SD.cs
@@ -20,5 +20,8 @@ public static class SD
     public const string Role_Customer = "Customer";
     
     public const string Local_Token = "JWT Token";
+    public const string Local_OrderDetails= "Local_OrderDetails";
     public const string Local_UserDetails= "UserDetails";
+
+
 }
