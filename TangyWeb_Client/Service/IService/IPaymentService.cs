1ddff0022d3864e00b7ff71aadbd1871c92acac1
gerardheuvelman <gerardheuvelman@gmail.com>
2023-07-26 13:00:38 +0200

    194. Payment Service in Blazor WASM

---
+++ TangyWeb_Client/Service/IService/IPaymentService.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.Design;
+using Tangy_Models;
+using TangyWeb_Client.ViewModels;
+
+namespace TangyWeb_Client.Service.IService;
+
+public interface IPaymentService
+{
+    public Task<SuccessModelDTO> Checkout(StripePaymentDTO model);
+
+
+}
